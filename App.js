0dccd6d634dbe10b81c0c6bf849752f959655434
Yemelyanov <nikolaef.post@gmail.com>
2023-10-26 15:38:22 +0300

    feat: completed

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, View} from 'react-native';
+import {useState} from "react";
+import {GoalItems, GoalInput} from "./components";
+import {StatusBar} from "expo-status-bar";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [modalIsVisible, setModalIsVisible] = useState(false);
+    const [goalsList, setGoalsList] = useState([]);
+
+    const toggleAddGoalHandler = () => {
+        setModalIsVisible(prev => !prev);
+    }
+
+    const addGoalHandler = (goalObj) => {
+        setGoalsList(prev => [...prev, goalObj]);
+        toggleAddGoalHandler()
+    }
+    const deleteGoalHandler = (id) => {
+        setGoalsList(prev => prev.filter(el => el.id !== id))
+    }
+
+    return (
+        <>
+            <StatusBar style="light" backgroundColor='black' />
+            <View style={styles.appContainer}>
+                <Button title="Add New Goal" color="#5e0acc" onPress={toggleAddGoalHandler}/>
+                <GoalInput
+                    addGoalHandler={addGoalHandler}
+                    modalIsVisible={modalIsVisible}
+                    toggleAddGoalHandler={toggleAddGoalHandler}
+                />
+                <GoalItems goalsList={goalsList} deleteGoalHandler={deleteGoalHandler}/>
+            </View>
+        </>
+    );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer: {
+        paddingVertical: 50,
+        paddingHorizontal: 16,
+        flex: 1
+    }
 });
